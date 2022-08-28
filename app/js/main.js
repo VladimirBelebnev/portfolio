a5881810e81c60e3e75a9df1bc28210a90727354
Vladimir Belebnev <94782107+VladimirBelebnev@users.noreply.github.com>
2022-08-28 22:50:04 +0300

    bug fix 0.15

--- app/js/main.js
+++ app/js/main.js
@@ -53,52 +53,7 @@ window.addEventListener('DOMContentLoaded', () => {
   \*************************************/
 /***/ (() => {
 
-const marque = () => {
-  const wrapper = document.querySelector('.skills'),
-        marqueeLeft = document.querySelector('.left-move'),
-        marqueeRight = document.querySelector('.right-move'),
-        wrapperWidth = wrapper.offsetWidth,
-        marqueeWidth = marqueeRight.scrollWidth;
 
-  function moveLeft() {
-    let currentTX = getComputedStyle(marqueeLeft).transform.split(',');
-
-    if (currentTX[4] === undefined) {
-      currentTX = -1;
-    } else {
-      currentTX = parseFloat(currentTX[4]) - 1;
-    }
-
-    if (-currentTX >= marqueeWidth) {
-      marqueeLeft.style.transform = 'translateX(' + wrapperWidth + 'px)';
-    } else {
-      marqueeLeft.style.transform = 'translateX(' + currentTX + 'px)';
-    }
-  }
-
-  function moveRight() {
-    let currentTX = getComputedStyle(marqueeRight).transform.split(',');
-
-    if (currentTX[4] === undefined) {
-      currentTX = -1;
-    } else {
-      currentTX = parseFloat(currentTX[4]) - 1;
-    }
-
-    if (-currentTX >= marqueeWidth) {
-      marqueeRight.style.transform = 'translateX(' + wrapperWidth + 'px)';
-    } else {
-      marqueeRight.style.transform = 'translateX(' + currentTX + 'px)';
-    }
-  }
-
-  setInterval(moveLeft, 1);
-  setInterval(moveRight, 1);
-};
-
-window.addEventListener('DOMContentLoaded', () => {
-  marque();
-});
 
 /***/ })
 
