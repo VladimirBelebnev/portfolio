7b4e462ad5a8f200d8d164c18b94bea9ca85ed81
Vladimir Belebnev <94782107+VladimirBelebnev@users.noreply.github.com>
2022-08-30 12:41:34 +0300

    compeated step and services section

--- app/js/main.js
+++ app/js/main.js
@@ -111,6 +111,44 @@ const slider = () => {
       }
     }
   });
+  const servicesSlider = new swiper__WEBPACK_IMPORTED_MODULE_0__["default"]('.services__slider', {
+    direction: 'horizontal',
+    loop: true,
+    speed: 500,
+    navigation: {
+      nextEl: '.services__next',
+      prevEl: '.services__prev'
+    },
+    pagination: {
+      el: '.services__pagination',
+      clickable: true
+    },
+    breakpoints: {
+      1681: {
+        slidesPerView: 3,
+        spaceBetween: 120
+      },
+      1401: {
+        slidesPerView: 3,
+        spaceBetween: 80
+      },
+      1171: {
+        slidesPerView: 3,
+        spaceBetween: 40
+      },
+      993: {
+        slidesPerView: 2,
+        spaceBetween: 50
+      },
+      769: {
+        slidesPerView: 2,
+        spaceBetween: 20
+      },
+      300: {
+        slidesPerView: 1
+      }
+    }
+  });
 };
 
 window.addEventListener('DOMContentLoaded', () => {
--- src/js/components/slider.js
+++ src/js/components/slider.js
@@ -25,6 +25,45 @@ const slider = () => {
             }
         }
     });
+    
+    const servicesSlider = new Swiper('.services__slider', {
+        direction: 'horizontal',
+        loop: true,
+        speed: 500,
+        navigation: {
+            nextEl: '.services__next',
+            prevEl: '.services__prev',
+        },
+        pagination: {
+            el: '.services__pagination',
+            clickable: true,
+        },
+        breakpoints: {
+            1681: {
+                slidesPerView: 3,
+                spaceBetween: 120,
+            },
+            1401: {
+                slidesPerView: 3,
+                spaceBetween: 80,
+            },
+            1171: {
+                slidesPerView: 3,
+                spaceBetween: 40,
+            },
+            993: {
+                slidesPerView: 2,
+                spaceBetween: 50,
+            },
+            769: {
+                slidesPerView: 2,
+                spaceBetween: 20,
+            },
+            300: {
+                slidesPerView: 1,
+            }
+        }
+    });
 };
 
 window.addEventListener('DOMContentLoaded', () => {
